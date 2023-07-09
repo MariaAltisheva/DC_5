57df805cbf878b3a4e2c30a57937b07a9d8dba59
maria <altysheva_maria@mail.ru>
2023-07-09 20:11:38 +0300

    SL

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ smiles = st.text_input('Введите SMILES', 'SMILES')
 st.write('Вы ввели', smiles)
 
 genus = st.number_input('Insert genus')
-st.write('The current number is ', genus)
+st.write('The current genus is ', genus)
 
 NP_Synthesis = st.number_input('Insert NP_Synthesis')
-st.write('The current number is ', NP_Synthesis)
+st.write('The current NP_Synthesis is ', NP_Synthesis)
 
 NP_Size_min = st.number_input('Insert NP_Size_min')
-st.write('The current number is ', NP_Size_min)
+st.write('The current NP_Size_min is ', NP_Size_min)
 
 NP_Size_avg = st.number_input('Insert NP_Size_avg')
-st.write('The current number is ', NP_Size_avg)
+st.write('The current NP_Size_avg is ', NP_Size_avg)
 
 avg_incup_period = st.number_input('Insert avg_incup_period')
-st.write('The current number is ', avg_incup_period)
+st.write('The current avg_incup_period is ', avg_incup_period)
 
 growth_temp = st.number_input('Insert growth_temp, C')
-st.write('The current number is ', growth_temp)
+st.write('The current growth_temp is ', growth_temp)
 
 ZOI_drug = st.number_input('Insert ZOI_drug')
-st.write('The current number is ', ZOI_drug)
+st.write('The current ZOI_drug is ', ZOI_drug)
 
 ZOI_NP = st.number_input('Insert ZOI_NP')
-st.write('The current number is ', ZOI_NP)
+st.write('The current ZOI_NP is ', ZOI_NP)
